478059b373f635ac103c1ed290c5bf6c121c4769
kenta-matsuzaki <kenta6910@gmail.com>
2019-08-08 10:05:42 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@
   let miss = 0;
   const timeLimit = 3 * 1000;
   let startTime;
+  let isPlaying = false;
   
   const target = document.getElementById('target');
   const scoreLabel = document.getElementById('score');
@@ -38,6 +39,7 @@
     }, 10);
 
     if (timeLeft < 0){
+      isPlaying = false;
       clearTimeout(timeoutId);
       timerLabel.textContent = '0.00';
       setTimeout(() => {
@@ -47,12 +49,19 @@
   }
 
   window.addEventListener('click', () => {
+    if (isPlaying === true){
+      return;
+    }
+    isPlaying = true;
     updateTarget();
     startTime = Date.now();
     updateTimer();
   });
 
   window.addEventListener('keyup', e => {
+    if (isPlaying !== true){
+      return;
+    }
     if (e.key === word[loc]){
       loc++;
       if (loc === word.length){
